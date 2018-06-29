6e9040f25009779ac7a01fd65d2a4880467ab0d5
Dimas Restu H <dimas.restu@student.upi.edu>
2018-06-29 16:01:50 +0700

    update api-simple, api-mysql, api-mongo

--- api-simple/main.go
+++ api-simple/main.go
@@ -28,10 +28,10 @@ func main() {
 
 	// Initialize Router Endpoint
 	router.HandleFunc("/", controllers.GetIndex).Methods("GET")
-	router.HandleFunc("/auth", controllers.GetAuthenticationJWT).Methods("POST")
+	router.HandleFunc("/auth", controllers.GetAuthentication).Methods("POST")
 
 	// Initialize Router Endpoint Secured With Basic Auth
-	router.Handle("/basic-auth", helpers.AuthBasic(controllers.GetAuthenticationJWT)).Methods("GET", "POST")
+	router.Handle("/basic-auth", helpers.AuthBasic(controllers.GetAuthentication)).Methods("GET", "POST")
 
 	// Initialize Router Endpoint Secured With Authorization
 	router.Handle("/users", helpers.AuthJWT(controllers.GetUser)).Methods("GET")
