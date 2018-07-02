5f1393d497fdef6b300056040c176a5480865330
Dimas Restu H <dimas.restu@student.upi.edu>
2018-07-02 22:16:33 +0700

    update api-mysql, api-mongo

--- api-mysql/controllers/c_index.go
+++ api-mysql/controllers/c_index.go
@@ -1,10 +1,8 @@
 package controllers
 
 import (
-	"encoding/json"
 	"net/http"
 
-	"github.com/dimaskiddo/simple-go/api-mysql/helpers"
 	"github.com/dimaskiddo/simple-go/api-mysql/routers"
 )
 
@@ -17,38 +15,4 @@ func GetIndex(w http.ResponseWriter, r *http.Request) {
 	response.Message = "Simple Go Programming Example (API MySQL)"
 
 	routers.ResponseWrite(w, response.Code, response)
-}
-
-// Function to Get JWT Authentication
-func GetAuthenticationJWT(w http.ResponseWriter, r *http.Request) {
-	var creds helpers.JWTCredentials
-
-	// Decode JSON from Request Body to User Data
-	// Use _ As Temporary Variable
-	_ = json.NewDecoder(r.Body).Decode(&creds)
-
-	// Make Sure Username and Passwor is Not Empty
-	if len(creds.Username) == 0 || len(creds.Password) == 0 {
-		routers.ResponseBadRequest(w)
-		return
-	}
-
-	// Some Business Logic Here to Match The Username and Password
-	if creds.Username == "user" && creds.Password == "password" {
-		// Get JWT Token From Pre-Defined Function
-		token, err := helpers.GetJWTToken(creds.Username)
-		if err != nil {
-			routers.ResponseInternalError(w)
-		} else {
-			var response helpers.JWTResponse
-
-			response.Status = true
-			response.Code = http.StatusOK
-			response.Token = token
-
-			routers.ResponseWrite(w, response.Code, response)
-		}
-	} else {
-		routers.ResponseUnauthorized(w)
-	}
 }
